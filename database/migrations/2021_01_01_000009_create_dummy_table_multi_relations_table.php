be174b1937dbfd80cdccd8453f0887ae9315553e
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2021-03-29 17:53:54 +0800

    Update DB

--- database/migrations/2021_01_01_000009_create_dummy_table_multi_relations_table.php
+++ database/migrations/2021_01_01_000009_create_dummy_table_multi_relations_table.php
@@ -4,23 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateDummyTableFiltersTable extends Migration {
+class CreateDummyTableMultiRelationsTable extends Migration {
   public function up() {
-    Schema::create('dummy_table_filters', function (Blueprint $table) {
+    Schema::create('dummy_table_multi_relations', function (Blueprint $table) {
       $table->increments('id');
-      $table->integer('id_multi_relation')->unsigned();
+      $table->integer('id_single_relation')->unsigned();
+      $table->timestamp('date_start')->nullable();
+      $table->timestamp('date_end')->nullable();
       $table->string('name');
       $table->text('description')->nullable();
       $table->integer('active')->default(1);
       $table->integer('status')->default(1);
       $table->integer('created_by')->nullable()->default('0');
       $table->integer('updated_by')->nullable()->default('0');
-      $table->foreign('id_multi_relation')->references('id')->on('dummy_table_multi_relations')->onDelete('restrict')->onUpdate('restrict');
+      $table->foreign('id_single_relation')->references('id')->on('dummy_table_single_relations')->onDelete('restrict')->onUpdate('restrict');
       $table->timestamps();
     });
   }
 
   public function down() {
-    Schema::dropIfExists('dummy_table_filters');
+    Schema::dropIfExists('dummy_table_multi_relations');
   }
 }
--- database/migrations/2021_01_01_000010_create_dummy_table_filters_table.php
+++ database/migrations/2021_01_01_000010_create_dummy_table_filters_table.php
@@ -4,23 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateDummyTableMultiRelationsTable extends Migration {
+class CreateDummyTableFiltersTable extends Migration {
   public function up() {
-    Schema::create('dummy_table_multi_relations', function (Blueprint $table) {
+    Schema::create('dummy_table_filters', function (Blueprint $table) {
       $table->increments('id');
-      $table->integer('id_single_relation')->unsigned();
+      $table->integer('id_multi_relation')->unsigned();
       $table->string('name');
+      
       $table->text('description')->nullable();
       $table->integer('active')->default(1);
       $table->integer('status')->default(1);
       $table->integer('created_by')->nullable()->default('0');
       $table->integer('updated_by')->nullable()->default('0');
-      $table->foreign('id_single_relation')->references('id')->on('dummy_table_single_relations')->onDelete('restrict')->onUpdate('restrict');
+      $table->foreign('id_multi_relation')->references('id')->on('dummy_table_multi_relations')->onDelete('restrict')->onUpdate('restrict');
       $table->timestamps();
     });
   }
 
   public function down() {
-    Schema::dropIfExists('dummy_table_multi_relations');
+    Schema::dropIfExists('dummy_table_filters');
   }
 }
