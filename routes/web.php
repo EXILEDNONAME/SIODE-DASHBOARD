d26c6681931dc32518d51f9aad2ec63f72f80fb0
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2021-03-30 16:38:22 +0700

    Update Profile

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,17 @@ require __DIR__.'/backend/system/management.php';
 // MAIN
 require __DIR__.'/backend/main/jasamarga.php';
 require __DIR__.'/backend/main/vms.php';
+
+// PROFILE
+Route::group([
+  'as' => 'dashboard.profile.',
+  'prefix' => 'dashboard/profile',
+  'namespace' => 'Backend\System',
+], function(){
+  Route::get('about', 'ProfileController@about')->name('about');
+  Route::get('{slug}/password', 'ProfileController@password')->name('password');
+  Route::post('{slug}/change-password', 'ProfileController@change_password')->name('change-password');
+  Route::get('{slug}/personal-information', 'ProfileController@personal_information')->name('personal-information');
+  Route::get('{slug}/timeline', 'ProfileController@timeline')->name('timeline');
+  Route::resource('/', 'ProfileController')->parameters(['' => 'id']);
+});
