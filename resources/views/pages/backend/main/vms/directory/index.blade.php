631e536a1d6e76156e994f3137456ca67d777f88
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2021-04-01 15:43:21 +0700

    Update

--- resources/views/pages/backend/main/vms/directory/index.blade.php
+++ resources/views/pages/backend/main/vms/directory/index.blade.php
@@ -18,3 +18,34 @@
 { data: 'mac_address' },
 { data: 'port' },
 @endpush
+
+@push('filter-header')
+<div class="col-md-2 my-2 my-md-0">
+  <div class="d-flex align-items-center">
+    {!! Form::select(NULL, filter_vms_areas(), NULL, ['data-column' => 2, 'placeholder' => '- Filter Area -', 'class' => 'form-control filter-area']) !!}
+  </div>
+</div>
+<div class="col-md-2 my-2 my-md-0">
+  <div class="d-flex align-items-center">
+    {!! Form::select(NULL, filter_vms_types(), NULL, ['data-column' => 2, 'placeholder' => '- Filter Type -', 'class' => 'form-control filter-type']) !!}
+  </div>
+</div>
+@endpush
+
+@push('filter-function')
+d.filter_area = $('#filter_area').val();
+d.filter_type = $('#filter_type').val();
+@endpush
+
+@push('filter-data')
+$('.filter-area').change(function () {
+  table.column(2)
+  .search( $(this).val() )
+  .draw();
+});
+$('.filter-type').change(function () {
+  table.column(3)
+  .search( $(this).val() )
+  .draw();
+});
+@endpush
