d88ca74b5f3057195608077920cef6f2f28ca160
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2021-04-09 17:04:56 +0800

    Update Page VMS

--- app/Http/Controllers/Backend/Main/Vms/MaintenanceController.php
+++ app/Http/Controllers/Backend/Main/Vms/MaintenanceController.php
@@ -87,10 +87,6 @@ class MaintenanceController extends Controller {
   **/
 
   public function store(Request $request) {
-    $validated = $request->validate([
-      'name' => 'required|unique:.'. $this->model . '|min:3',
-    ]);
-
     $store = $request->all();
     $this->model::create($store);
     return redirect($this->url)->with('success', trans('default.notification.success.item-created'));
@@ -115,10 +111,6 @@ class MaintenanceController extends Controller {
   **/
 
   public function update(Request $request, $id) {
-    $validated = $request->validate([
-      'name' => 'required|unique:accesses,name,'.$id,
-    ]);
-
     $data = $this->model::findOrFail($id);
     $update = $request->all();
     $data->update($update);
